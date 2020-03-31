c4dd956ba5375017132ba0c68e0c81fa90c1193e
HarshJoshi123 <harsh2606joshi@gmail.com>
2020-03-31 12:16:18 +0530

    second

--- src/component/Header.js
+++ src/component/Header.js
@@ -3,7 +3,7 @@ import {Link,withRouter } from "react-router-dom"
 
  const isactive=(history,path)=>{
    if(history.location.pathname===path){ 
-return {color:"#ff9900",fontSize:"140%"}
+return {color:"#ff9900",fontSize:"140%",fontFamily:"Alfa Slab One"}
 }
    else  return {color:"#ffffff",fontSize:"90%"}
 
--- src/component/Menu.js
+++ src/component/Menu.js
@@ -80,7 +80,7 @@ if(this.state.redirectToBilling){
 const {cart}=this.state
 
   return(<div  class="container jumbotron" >
-   <h1 style={{textAlign:"center",fontFamily: "Amatic SC",fontWeight:"bolder",letterSpacing:"5px"}}>MENU </h1>  
+   <h1 style={{fontFamily:"sofia",textAlign:"center",fontWeight:"bolder",letterSpacing:"8px"}}>MENU </h1>  
 { cart[0] && (<div style={{display:"flex"}}class="alert alert-success" role="alert">
   You have {cart.length} Items In Your Cart &nbsp; <span style={{fontWeight:"bolder"}}>  {this.state.total} &#8377; (Without Taxes) </span> 
   <button style={{marginLeft:"auto"}} onClick={this.Checkout} class="btn btn-primary btn-lg active" role="button"> CHECKOUT </button>
@@ -99,7 +99,7 @@ const {cart}=this.state
           <div id="div-card-image">
           <img src={v.path} id="image-card" /> 
         </div>
-       <section style={{display:"flex",flexDirection:"column",flexWrap:"wrap"}} className="Prices"> <h2 style={{flexGrow:"2",backgroundColor:"MediumSeaGreen",textAlign:"center"}}>{v.name} </h2> 
+       <section style={{display:"flex",flexDirection:"column",flexWrap:"wrap"}} className="Prices"> <h2 style={{flexGrow:"2",backgroundColor:"MediumSeaGreen",textAlign:"center",fontFamily:"Alex Brush"}}>{v.name} </h2> 
        <div style={{flexGrow:"1",display:"flex",flexWrap:"wrap"}}> {v.Qty.map((qty,j)=>{return(<div style={{flexGrow:"1",paddingBottom:"10px"}}> &nbsp; {qty} ({v.prices[j]} &#8377;) :<input type="number" id="quantity"  onChange={this.handlechange.bind(this,qty,i)} name="quantity" placeholder="0" min="0" max="5" />  </div>)})}   </div>
       
       </section>
